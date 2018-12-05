5cb7d9901885bbd0236c3a10bf10a9f66e489654
unknown <rwggad@naver.com>
2018-12-05 18:10:44 +0900

    example1 add

---
+++ src/main/java/TranspotationWalk.java
@@ -0,0 +1,5 @@
+public class TranspotationWalk {
+    public void move(){
+        System.out.println("도보로 이동 합니다.");
+    }
+}
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,5 +1,24 @@
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+import sun.net.www.content.text.Generic;
+
 public class main {
     public static void main(String[] argc){
 
+        /**
+         * 일반적으로 자바에서 객체를 사용할때.. 모듈을 사용 x */
+        //TranspotationWalk transpotationWalk = new TranspotationWalk();
+        //transpotationWalk.move();
+
+        /**
+         * Spring에서 applicationContext에서 로딩 시켜놓은 Bean으로 사용 */
+        // 일단 Spring Container에 접근한다
+        GenericXmlApplicationContext context = new GenericXmlApplicationContext("classpath:applicationContext.xml");
+        // applicationContext.xml 에서 지정한 Bean을 가져온다. (id 이름, 데이터 타입)
+        TranspotationWalk transpotationWalk;
+        transpotationWalk = context.getBean("tWalk", TranspotationWalk.class);
+        transpotationWalk.move();
+        // 리소스 반환
+        context.close();
     }
 }
