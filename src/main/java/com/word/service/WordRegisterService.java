3d7210ba18569134ce3b1d8237ece43628cf6e26
unknown <rwggad@naver.com>
2018-12-06 15:47:48 +0900

    example5 add comment

---
+++ src/main/java/com/word/service/WordRegisterService.java
@@ -0,0 +1,33 @@
+package com.word.service;
+
+import com.word.WordSet;
+import com.word.dao.WordDao;
+
+public class WordRegisterService {
+   /**
+	* 단어 등록*/
+	private WordDao wordDao;
+	
+	public WordRegisterService(WordDao wordDao) {
+		this.wordDao = wordDao;
+	}
+	
+	public void register(WordSet wordSet) {
+		String wordKey = wordSet.getWordKey();
+		if(verify(wordKey)) {
+			wordDao.insert(wordSet);
+		} else {
+			System.out.println("The word has already registered.");
+		}
+	}
+	
+	public boolean verify(String wordKey){
+		WordSet wordSet = wordDao.select(wordKey);
+		return wordSet == null ? true : false;
+	}
+	
+	public void setWordDao(WordDao wordDao) {
+		this.wordDao = wordDao;
+	}
+	
+}
